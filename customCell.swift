fe3877165dccc7ea70b98279519e501effdad89c
kishimotsu72 <kishimotsu72@gmail.com>
2018-08-01 10:51:03 +0900

    Initial Commit

---
+++ customCell.swift
@@ -0,0 +1,23 @@
+//
+//  customCell.swift
+//  
+//
+//  Created by きしもつ on 2018/07/25.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
